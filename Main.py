dda7ed845b32dfa23992c863d0cec9d3427eca36
Shantonu Acharjee <shantonuacharjee@gmail.com>
2022-02-19 17:38:54 +0600

    Basic Done

--- Main.py
+++ Main.py
@@ -1,18 +1,24 @@
 import pyshorteners
-ProductLink = input('Enter Your ProductLink : ')
 
-if '?' in ProductLink:
-    index = ProductLink.find('?')
-    ProductLink = ProductLink[0: index]
+while True:
+
+    ProductName = input('\n\n\nEnter Your ProductName : ')
+    ProductLink = input('\n\n\nEnter Your ProductLink : ')
+
+    if '?' in ProductLink:
+        index = ProductLink.find('?')
+        ProductLink = ProductLink[0: index]
 
 
-print('\n\n\nThe Main Product Link Is : ', ProductLink)
+    print('\n\n\nThe Main Product Link Is : ', ProductLink)
 
-Link = ProductLink + \
-    '?tracking=APvA30dTUiUIbOl0hhFQOnWOXP0R1o4dp6iTLjXbneYGDB5fzzTxREhinCfDIDVV'
-print('\n\n\nThe Affiliate Link Is : ', Link)
+    Link = ProductLink + \
+        '?tracking=APvA30dTUiUIbOl0hhFQOnWOXP0R1o4dp6iTLjXbneYGDB5fzzTxREhinCfDIDVV'
+    print('\n\n\nThe Affiliate Link Is : ', Link)
 
-shortener = pyshorteners.Shortener()
-ShotLink = shortener.tinyurl.short(Link)
+    shortener = pyshorteners.Shortener()
+    ShotLink = shortener.tinyurl.short(Link)
 
-print('\n\n\nThe Short Link Is : ', ShotLink)
+    print(f'\n\n\nThe Short Link Is -----> {ProductName} :- ', ShotLink)
+
+   
